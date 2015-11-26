83e11956525cb535e7da8162a9080749836b98b8
Aji Indrayana <ajivancouver@gmail.com>
2015-11-25 19:40:05 -0800

    adding mailer feature when user post a comment

---
+++ blog-v2/config/initializers/setup_mail.rb
@@ -0,0 +1,10 @@
+
+# this code could be find from rails guide
+ActionMailer::Base.smtp_settings = {
+  address:              "smtp.gmail.com",
+  port:                 "587",
+  enable_starttls_auto: true,
+  authentication:       :plain,
+  user_name:            ENV["email_user_name"],
+  password:             ENV["email_password"]
+}
